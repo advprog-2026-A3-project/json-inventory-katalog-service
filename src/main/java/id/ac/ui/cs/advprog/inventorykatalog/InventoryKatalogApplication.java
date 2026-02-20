cd9172eb60531d4f2bec2efde53f81a50cb69382
abhivadya1 <abhivadya1@gmail.com>
2026-02-20 12:56:03 +0700

    init springboot

---
+++ src/main/java/id/ac/ui/cs/advprog/inventorykatalog/InventoryKatalogApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.inventorykatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryKatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryKatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/inventorykatalog/InventoryKatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.inventorykatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryKatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
